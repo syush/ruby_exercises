c32f874c5a0b2bdc42c567bcbf0fdeb9c3c87208
Sergey Shishlov <shishlov@gmail.com>
2015-09-18 12:45:38 +0300

    Typo fixed

--- lesson_2/exercise_4.rb
+++ lesson_2/exercise_4.rb
@@ -1,6 +1,6 @@
 puts "Welcome to triangle area calculation!"
 puts "Please enter the length of the a side (base):"
 a = gets.chomp.to_f
-puts "Please enter the heigth:"
+puts "Please enter the height:"
 h = gets.chomp.to_f
 puts "The area equals #{a * h / 2}."
