d6266c76278bd1fe071706b7e37e0021f40c23f6
Sergey Shishlov <shishlov@gmail.com>
2015-09-18 12:40:04 +0300

    Lesson 2, exercise 3

---
+++ lesson_2/exercise_3.rb
@@ -0,0 +1,6 @@
+puts "Welcome to rectangle area calculation!"
+puts "Please enter the length of the a side:"
+a = gets.chomp.to_f
+puts "Please enter the length of the b side:"
+b = gets.chomp.to_f
+puts "The area equals #{a * b}."
