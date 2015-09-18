4f1d3bdd980ee8c1e86590d049302cadfcf22423
Sergey Shishlov <shishlov@gmail.com>
2015-09-18 12:37:17 +0300

    Lesson 2, exercise 2

---
+++ lesson_2/exercise_2.rb
@@ -0,0 +1,6 @@
+puts "Welcome to rectangle perimeter calculation!"
+puts "Please enter the length of the a side:"
+a = gets.chomp.to_f
+puts "Please enter the length of the b side:"
+b = gets.chomp.to_f
+puts "The perimeter equals #{(a + b) * 2}."
