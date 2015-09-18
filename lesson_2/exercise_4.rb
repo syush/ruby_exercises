deed272b1a8e5340d619dc8a17a45e92ec65cc16
Sergey Shishlov <shishlov@gmail.com>
2015-09-18 12:42:51 +0300

    Lesson 2, exercise 4

---
+++ lesson_2/exercise_4.rb
@@ -0,0 +1,6 @@
+puts "Welcome to triangle area calculation!"
+puts "Please enter the length of the a side (base):"
+a = gets.chomp.to_f
+puts "Please enter the heigth:"
+h = gets.chomp.to_f
+puts "The area equals #{a * h / 2}."
