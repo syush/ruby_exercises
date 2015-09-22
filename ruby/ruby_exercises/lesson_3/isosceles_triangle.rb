3dcf0a41cad03b1f1c745e05b9f2502c8737c23c
Sergey Shishlov <shishlov@gmail.com>
2015-09-22 15:11:15 +0300

    Rightangled triangle detection

--- ruby/ruby_exercises/lesson_3/isosceles_triangle.rb
+++ ruby/ruby_exercises/lesson_3/isosceles_triangle.rb
@@ -1,4 +1,4 @@
-puts "Let's check whether a triangle is isolateral or isosceles."
+puts "Let's check whether a triangle is isolateral/isosceles/rightangled."
 puts "Enter the length of the 1st side:"
 a = gets.chomp.to_f
 puts "Enter the length of the 2nd side:"
@@ -12,3 +12,23 @@ elsif (a == b) || (b == c) || (a == c)
 else
   puts "The triangle is neither isolateral nor isosceles (не является ни равнобедренным, ни равносторонним)."
 end
+if (a > b) && (a > c) 
+  hyp = a
+  cat1 = b
+  cat2 = c
+elsif (b > a) && (b > c)
+  hyp = b
+  cat1 = a
+  cat2 = c
+elsif (c > a) && (c > b)
+  hyp = c
+  cat1 = a
+  cat2 = b
+end
+if hyp && (hyp**2 == cat1**2 + cat2**2)
+  puts "The triangle is rightangled (прямоугольный)."
+else
+  puts "The triangle is not rightangled (не является прямоугольным)."
+end
+  
+  
