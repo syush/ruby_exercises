03db7a835a6d60661e06a380b5a82cbdd906ad33
Sergey Shishlov <shishlov@gmail.com>
2015-09-25 16:06:04 +0300

    prefix condition replaced by postfix

--- lesson_4/month_hash.rb
+++ lesson_4/month_hash.rb
@@ -2,9 +2,5 @@ puts "This program creates a hash with the number of days in a month and prints 
 days_per_month = {January:31, February:28, March:31, April:30, May:31, June:30, 
                   July:31, August:31, September:30, October:31, November:30, December:31}
 puts "The list of such months is below:"
-days_per_month.each do |key,value|
-  if value == 30
-    print "#{key} "
-  end
-end
+days_per_month.each { |key,value|  print "#{key} " if value == 30 }
 puts
