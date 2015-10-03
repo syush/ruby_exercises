4929c24f342a777753b3ef43aeb7ac69b0182bef
Sergey Shishlov <shishlov@gmail.com>
2015-10-03 06:12:54 +0300

    Cargo load control

--- lesson_6/cargo_car.rb
+++ lesson_6/cargo_car.rb
@@ -2,10 +2,25 @@ require_relative 'car'
   
 class CargoCar < Car
 
-  def initialize(max_load)
+  def initialize(capacity)
     super
     @type = :cargo
-    @max_load = max_load
+    @free_space = @capacity = capacity.to_f
   end
 
+  def load(volume)
+    if @free_space < volume
+      puts "Error: can't load #{volume} m^3; only #{@free_space} m^3 of room is available"
+    else
+      @free_space -= volume
+    end
+  end
+
+  def unload(volume)
+    if @capacity - @free_space < volume
+      puts "Error: can't unload #{volume} m^3; the car contains only #{@capacity - @free_space} m^3 of cargo"
+    else
+      @free_space += volume
+    end
+  end
 end
